fe8128c7e409c5ad69c3dc8e813b9dd1415048a8
venkat74496 <109568466+venkat74496@users.noreply.github.com>
2022-07-20 19:37:57 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,82 @@
-#st=y.execute("create table student (first_name text, last_name text, dob text, gender text, email text, phonenumber int, parents_name text, phonenumber_parents int, address_line1 text, address_line2 text, landmark text,pincode int, tenth_school_name text, tenth_mark int, stream text, twelth_school_name text, twelth_mark int)")
-import sqlite3 as s
-y=s.connect("student.db")
-def storedata(x):
-	#st=y.execute("insert into student (first_name, last_name, dob, gender, email, phonenumber, parents_name, phonenumber_parents, address_line1, address_line2, landmark, pincode, tenth_school_name, tenth_mark, stream, twelth_school_name, twelth_mark) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(x["firstName"], x["lastName"], x["birthdayDate"], x["inlineRadioOptions"], x["email"], x["phoneNumber"], x["parents_name"], x["phoneNumber_p"], x["Address1"], x["Address2"], x["Landmark"], x["Pincode"], x["ten_school_name"], x["ten_mark"], x["stream"], x["twel_school_name"], x["twel_mark"]))
-	print(x)
+import mysql.connector
+mydb = mysql.connector.connect(host="localhost", user="root",  password="",  database = "Venkat")
+def count(table):
+    mycursor = mydb.cursor()
+    e="SELECT count(*) from "+table
+    mycursor.execute(e)
+    myresult = mycursor.fetchall()
+    mydb.close()
+    return myresult
+def storestudent(x):
+    mydb = mysql.connector.connect(host="localhost", user="root",  password="",  database = "Venkat")
+    r=count("login")
+    y=[r[0][0]]
+    for e in x.keys():
+        y.append(x[e])
+    y.append("student")
+    y=tuple(y)
+    d="INSERT INTO login(id, name,regno,batch, email, PASSWORD, role) VALUES  (%s,%s,%s,%s,%s,%s,%s)"
+    print(d)
+    mycursor = mydb.cursor()
+    mycursor.execute(d,y)
+    mydb.commit()
+    n=tuple(y[2].split())
+    d="INSERT INTO "+y[3]+"(regno) VALUES  (%s)"
+    print(d)
+    mycursor = mydb.cursor()
+    mycursor.execute(d,n)
+    mydb.commit()
+    mydb.close()
+    mydb.close()
+def login(user,pas):
+    mydb = mysql.connector.connect(host="localhost", user="root",  password="",  database = "Venkat")
+    mycursor = mydb.cursor()
+    e="SELECT name,role,email FROM login where email='%s' and password='%s'"%(user,pas)
+    mycursor.execute(e)
+    myresult = mycursor.fetchall()
+    mydb.close()
+    return myresult
+def currentdat():
+    import datetime 
+    return str(datetime.datetime.now())[0:10] 
+def stores(x,code,em):
+    mydb = mysql.connector.connect(host="localhost", user="root",  password="",  database = "Venkat")
+    mycursor = mydb.cursor()
+    r=x["sub"]+'_'+currentdat()[-2:]+'_'+currentdat()[-5:-3]+'_'+x["time"]
+    e="alter table "+x["batch"]+" add "+r+" varchar(50)"
+    mycursor.execute(e)
+    y=tuple([em,code,r])
+    d="INSERT INTO fcode(facultymail, code, gentime) VALUES  (%s,%s,%s)"
+    print(d)
+    mycursor = mydb.cursor()
+    mycursor.execute(d,y)
+    mydb.commit()
+    mydb.close()
+def storeats(x,em):
+    
+    mydb = mysql.connector.connect(host="localhost", user="root",  password="",  database = "Venkat")
+    mycursor = mydb.cursor()
+    e="SELECT  batch,regno FROM login where email='%s' "%(em)
+    mycursor.execute(e)
+    myresult = mycursor.fetchall()
+    table=(myresult[0][0])
+    rid=(myresult[0][1])
+    print(table,rid)
+    mycursor = mydb.cursor()
+    e="SELECT gentime  FROM fcode where code='%s' "%(x["sub"])
+    mycursor.execute(e)
+    myresult = mycursor.fetchall()
+    col=(myresult[0][0])
+    print(col)
+    y=tuple([rid])
+    mycursor = mydb.cursor()
+    d="update "+table+" set "+col+"='present' where regno = %s" 
+    print(d)
+    mycursor.execute(d,y)
+    mydb.commit()
+    mydb.close()
+
+    
+
+
+    
